6111a747391ffcacdaeaa753857c6de1d7332981
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-04-11 16:01:35 +0200

    feat(auth): integrate login page and secure requests routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { NotFound } from './core/pages/not-found/not-found';
 import { Shell } from './core/layout/shell/shell';
+import { Login } from './features/auth/login/login';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
+  { path: '', component: Login },
   {
-    path: '',
+    path: 'requests',
     component: Shell,
+    canActivate: [authGuard],
     loadChildren: () => import('./features/requests/requests.routes').then((m) => m.requestsRoutes),
   },
   { path: '**', component: NotFound },
--- src/app/features/requests/requests.routes.ts
+++ src/app/features/requests/requests.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const requestsRoutes: Routes = [
-  { path: '', loadComponent: () => import('./overview/overview').then((m) => m.Overview) },
+  {
+    path: '',
+    redirectTo: 'overview',
+    pathMatch: 'full',
+  },
+  {
+    path: 'overview',
+    loadComponent: () => import('./overview/overview').then((m) => m.Overview),
+  },
   { path: 'new', loadComponent: () => import('./form/form').then((m) => m.Form) },
 ];
